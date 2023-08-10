beb0e3b1f603e238ea15dbc29d44d8e62a42e6b0
kosne1 <akolia2017@list.ru>
2023-08-10 13:36:25 +0300

    feat(start keyboard): send_help_message_button

--- app/keyboards/start/__init__.py
+++ app/keyboards/start/__init__.py
@@ -1,10 +1,11 @@
 from telebot.types import InlineKeyboardMarkup
 
-from app.keyboards.start.button import make_order_button, search_order_button
+from app.keyboards.start.button import make_order_button, search_order_button, send_help_message_button
 
 customer_start_keyboard = InlineKeyboardMarkup()
 customer_start_keyboard.row(make_order_button)
-
+customer_start_keyboard.row(send_help_message_button)
 
 executor_start_keyboard = InlineKeyboardMarkup()
 executor_start_keyboard.row(search_order_button)
+executor_start_keyboard.row(send_help_message_button)
--- app/keyboards/start/text.py
+++ app/keyboards/start/text.py
@@ -2,3 +2,4 @@ from app.schemas.ButtonInfoSchema import ButtonInfo
 
 make_order_button_info = ButtonInfo(text="Оставить заказ", callback_data="make_order")
 search_order_button_info = ButtonInfo(text="Поиск заказа", callback_data="search_order")
+send_help_message_button_info = ButtonInfo(text="Написать в поддержку", callback_data="help")
