f754a5271fbac11a48ed52294566d1166e5cd854
kosne1 <akolia2017@list.ru>
2023-12-11 05:13:06 +0300

    feat(auth.handler): can create user with car

--- app/handlers/auth/__init__.py
+++ app/handlers/auth/__init__.py
@@ -1,10 +1,10 @@
 import datetime
 
-from telebot.types import CallbackQuery, Message, ReplyKeyboardRemove
+from telebot.types import CallbackQuery, Message, ReplyKeyboardRemove, PollAnswer
 
 from app import bot
 from app.api.User import Users
-from app.helpers import send_wait_message
+from app.helpers import send_wait_message, get_car_list
 from app.keyboards.first_start import first_start_keyboard
 from app.keyboards.first_start.text import register_button_info, sign_in_button_info
 from app.keyboards.phone_number import share_contact_keyboard
@@ -40,7 +40,8 @@ def register(call: CallbackQuery):
 
             def register_contact(message: Message):
                 log_file = open("info.log", "a")
-                log_file.write(f"\n[INFO {datetime.datetime.now()}]: {message.from_user.username} {message.contact.phone_number}, {type(message.contact.phone_number)}\n")
+                log_file.write(
+                    f"\n[INFO {datetime.datetime.now()}]: {message.from_user.username} {message.contact.phone_number}, {type(message.contact.phone_number)}\n")
                 log_file.close()
                 auth_storage_service.set_phone_number(chat_id=message.chat.id, user_id=message.from_user.id,
                                                       phone_number=message.contact.phone_number)
@@ -49,21 +50,43 @@ def register(call: CallbackQuery):
 
                 user_dto = auth_storage_service.get_create_user_dto(chat_id=message.chat.id,
                                                                     telegram_id=message.from_user.id)
+                if user_dto.role == 'executor':
+                    bot.send_poll(question='Каким авто вы владеете?', chat_id=message.chat.id,
+                                  is_anonymous=False,
+                                  allows_multiple_answers=True,
+                                  options=['Платформа', "Сдвижная платформа", "Манипулятор", "Автовоз"],
+                                  reply_markup=ReplyKeyboardRemove())
+                    return
                 user = Users().create(user_dto)
                 auth_storage_service.set_user_auth_id(chat_id=message.chat.id, user_id=message.from_user.id,
                                                       id=user.id)
 
                 auth_storage_service.set_state_registered(chat_id=message.chat.id, user_id=message.from_user.id)
 
-                new_reply_markup = customer_start_keyboard if user.role.type == 'customer' else executor_start_keyboard
                 bot.send_message(chat_id=message.chat.id, text='Регистрация успешно завершена!',
                                  reply_markup=ReplyKeyboardRemove())
                 bot.send_message(chat_id=message.chat.id, text='Добро пожаловать!',
-                                 reply_markup=new_reply_markup)
+                                 reply_markup=customer_start_keyboard)
 
             bot.register_next_step_handler(message, register_contact)
 
         bot.register_next_step_handler(call.message, register_fullname)
+
+
+@bot.poll_answer_handler(func=lambda call: True)
+def handle_poll_answer(pollAnswer: PollAnswer):
+    user_dto = auth_storage_service.get_create_user_dto(chat_id=pollAnswer.user.id,
+                                                        telegram_id=pollAnswer.user.id,
+                                                        car_list=get_car_list(pollAnswer.option_ids))
+    user = Users().create(user_dto)
+    auth_storage_service.set_user_auth_id(chat_id=pollAnswer.user.id, user_id=pollAnswer.user.id,
+                                          id=user.id)
+    auth_storage_service.set_state_registered(chat_id=pollAnswer.user.id, user_id=pollAnswer.user.id)
+
+    bot.send_message(chat_id=pollAnswer.user.id, text='Регистрация успешно завершена!',
+                     reply_markup=ReplyKeyboardRemove())
+    bot.send_message(chat_id=pollAnswer.user.id, text='Добро пожаловать!',
+                     reply_markup=executor_start_keyboard)
 
 
 @bot.callback_query_handler(func=lambda call: sign_in_button_info.filter(call.data))
